440aebd3912ae3102cd428957e7673a38bb969b7
ARNOLD D. JALANDO-ON <jalandoon.arnold777@gmail.com>
2023-12-11 01:38:12 +0800

    TechnicalExam6
    Boys
    Clariza, Mark Carlo S.
    Equiron, Justin Venedict D.
    Jalando-on, Arnold D.
    
    Girls
    Estiller, Andrea S.
    Olindo, Althea Kristah A.

---
+++ real-time-chat/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const server = require("http").createServer(app)
+
+const io = require("socket.io")(server);
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+io.on("connection", function(socket){
+    socket.on("newuser", function(username){
+        socket.broadcast.emit("update", username + " has joined the chat");
+    });
+    socket.on("exituser", function(username){
+        socket.broadcast.emit("update", username + " has left the chat");
+    });
+    socket.on("chat", function(message){
+        socket.broadcast.emit("chat", message);
+    });
+});
+
+// server.listen(3000);
+
+const PORT = process.env.PORT || 7000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
